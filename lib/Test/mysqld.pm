927831fcce4e2a14e2c703d9770149440f88db1b
Kazuho Oku <kazuhooku@gmail.com>
2009-10-15 15:19:47 +0900

    incr version, update changes (.pm was not updated in 0.08) orz

--- lib/Test/mysqld.pm
+++ lib/Test/mysqld.pm
@@ -11,7 +11,7 @@ use File::Temp qw(tempdir);
 use POSIX qw(SIGTERM WNOHANG);
 use Time::HiRes qw(sleep);
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 our $errstr;
 our @SEARCH_PATHS = qw(/usr/local/mysql);
