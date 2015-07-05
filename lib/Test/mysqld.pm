c11007d707ee66db017963476a131234c421ea93
Ichinose Shogo <shogo82148@gmail.com>
2015-07-05 23:07:47 +0900

    refactoring `start` and `stop`

--- lib/Test/mysqld.pm
+++ lib/Test/mysqld.pm
@@ -98,6 +98,14 @@ sub start {
     my $self = shift;
     return
         if defined $self->pid;
+    $self->spawn;
+    $self->wait_for_setup;
+}
+
+sub spawn {
+    my $self = shift;
+    return
+        if defined $self->pid;
     open my $logfh, '>>', $self->base_dir . '/tmp/mysqld.log'
         or die 'failed to create log file:' . $self->base_dir
             . "/tmp/mysqld.log:$!";
@@ -117,13 +125,20 @@ sub start {
         die "failed to launch mysqld:$?";
     }
     close $logfh;
+    $self->pid($pid);
+}
+
+sub wait_for_setup {
+    my $self = shift;
+    return
+        unless defined $self->pid;
+    my $pid = $self->pid;
     while (! -e $self->my_cnf->{'pid-file'}) {
         if (waitpid($pid, WNOHANG) > 0) {
             die "*** failed to launch mysqld ***\n" . $self->read_log;
         }
         sleep 0.1;
     }
-    $self->pid($pid);
     { # create 'test' database
         my $dbh = DBI->connect($self->dsn(dbname => 'mysql'))
             or die $DBI::errstr;
@@ -135,11 +150,24 @@ sub start {
 sub stop {
     my ($self, $sig) = @_;
 
-    local $?; # waitpid may change this value :/
+    return
+        unless defined $self->pid;
+    $sig ||= SIGTERM;
+    $self->send_stop_signal($sig);
+    $self->wait_for_stop;
+}
+
+sub send_stop_signal {
+    my ($self, $sig) = @_;
     return
         unless defined $self->pid;
     $sig ||= SIGTERM;
     kill $sig, $self->pid;
+}
+
+sub wait_for_stop {
+    my $self = shift;
+    local $?; # waitpid may change this value :/
     while (waitpid($self->pid, 0) <= 0) {
     }
     $self->pid(undef);
@@ -246,48 +274,11 @@ sub start_mysqlds {
 
     my @mysqlds = map { Test::mysqld->new(@args, auto_start => 0) } (1..$number);
     for my $mysqld (@mysqlds) {
-        # (re)create directory structure
-        mkdir $mysqld->base_dir;
-        for my $subdir (qw/etc var tmp/) {
-            mkdir $mysqld->base_dir . "/$subdir";
-        }
-
-        open my $logfh, '>>', $mysqld->base_dir . '/tmp/mysqld.log'
-            or die 'failed to create log file:' . $mysqld->base_dir
-                . "/tmp/mysqld.log:$!";
-        my $pid = fork;
-        die "fork(2) failed:$!" unless defined $pid;
-        if ($pid == 0) {
-            $mysqld->setup;
-
-            open STDOUT, '>&', $logfh
-                or die "dup(2) failed:$!";
-            open STDERR, '>&', $logfh
-                or die "dup(2) failed:$!";
-            exec(
-                $mysqld->mysqld,
-                '--defaults-file=' . $mysqld->base_dir . '/etc/my.cnf',
-                '--user=root',
-            );
-            die "failed to launch mysqld:$?";
-        }
-        close $logfh;
-        $mysqld->pid($pid);
+        $mysqld->setup;
+        $mysqld->spawn;
     }
-
     for my $mysqld (@mysqlds) {
-        while (! -e $mysqld->my_cnf->{'pid-file'}) {
-            if (waitpid($mysqld->pid, WNOHANG) > 0) {
-                die "*** failed to launch mysqld ***\n" . $mysqld->read_log;
-            }
-            sleep 0.1;
-        }
-
-        # create 'test' database
-        my $dbh = DBI->connect($mysqld->dsn(dbname => 'mysql'))
-            or die $DBI::errstr;
-        $dbh->do('CREATE DATABASE IF NOT EXISTS test')
-            or die $dbh->errstr;
+        $mysqld->wait_for_setup;
     }
     return @mysqlds;
 }
@@ -296,16 +287,11 @@ sub stop_mysqlds {
     my $class = shift;
     my @mysqlds = @_;
 
-    local $?; # waitpid may change this value :/
-
     for my $mysqld (@mysqlds) {
-        next unless $mysqld->pid;
-        kill SIGTERM, $mysqld->pid;
+        $mysqld->send_stop_signal;
     }
     for my $mysqld (@mysqlds) {
-        while (waitpid($mysqld->pid, 0) <= 0) {
-        }
-        $mysqld->pid(undef);
+        $mysqld->wait_for_stop;
     }
     return @mysqlds;
 }
