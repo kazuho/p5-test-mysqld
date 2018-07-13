00174a1733a6dc7e1949f8d53a24742471dc7950
Songmu <y.songmu@gmail.com>
2018-07-14 00:54:24 +0900

    add author/test-mysqld-runner.pl

---
+++ author/test-mysqld-runner.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use utf8;
+use autodie;
+
+use Test::mysqld;
+
+my $mysqld = Test::mysqld->new(
+    auto_start => undef,
+    my_cnf => {
+        'skip-networking' => '', # no TCP socket
+    },
+) or die $Test::mysqld::errstr;
+
+$SIG{TERM} = $SIG{INT} = sub {
+    undef $mysqld;
+    exit;
+};
+
+$mysqld->setup;
+
+print STDERR "runner_pid:\n";
+print STDERR "  $$\n";
+print STDERR "base_dir:\n";
+print STDERR "  @{[$mysqld->base_dir]}\n";
+print STDERR "socket:\n";
+print STDERR "  @{[$mysqld->my_cnf->{socket}]}\n";
+
+$mysqld->start;
+
+1 while 1;
